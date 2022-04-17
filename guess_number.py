c6b3c2e5ef17d0f668beac5f253e84a1f313c803
Busayo <akannibusayo@gmail.com>
2022-04-17 21:38:09 +0100

    Adding new version of guessing game

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,12 @@
 import random
 
 
-def game(stop):
+def game(x):
   start = 0
   number_guessed = 0
-  guess = random.randint(start, stop)
+  guess = random.randint(start, x)
   while number_guessed != guess:
-    number_guessed = int(input(f'Guess a number between 1 and {stop}: '))
+    number_guessed = int(input(f'Guess a number between 1 and {x}: '))
     if number_guessed < guess:
       print(f'Number was too low, please try a higher value')
     elif number_guessed > guess:
